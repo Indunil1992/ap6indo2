6c386681edf825843819882e1c200b3917b6ec52
Indunil1992 <indunil@adroitlogic.com>
2020-04-06 15:00:56 +0530

    1

--- ap6indo2.js
+++ ap6indo2.js
@@ -1,5 +1,21 @@
+let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
+
 exports.handler = async (event) => {
-    
-    return {"message": "successfully executed"};
-   
+    try {
+        let data = await sns.subscribe({
+            TopicArn: "arn:aws:sns:us-east-1:318300609668:TestSNS",
+            Protocol: "https",
+            Endpoint: "https://052p9kjn18.execute-api.us-east-1.amazonaws.com/test/qa/indu"
+        }).promise();
+console.log("Daaata")
+console.log(data)
+    } catch (err) {
+        console.log("errrr")
+console.log(err)
+        // error handling goes here
+    };
+
+    return { "message": "successfully executed" };
+
 };
